f0cc6aa53b8e10fd65a46772ad86d96f92f46637
Kevin <kevyekt@gmail.com>
2024-03-23 10:29:36 -0400

    added heroku access to server.js

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require('express');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes.js');
-const protectedRoutes = require('./routes/protectedRoutes.js'); // Import the protected routes
+const protectedRoutes = require('./routes/protectedRoutes.js');
 const workoutRoutes = require('./routes/workoutRoutes.js');
 const progressRoutes = require('./routes/progressRoutes.js');
-const nutritionRoutes = require('./routes/nutritionRoutes.js'); // Adjust the path as needed
+const nutritionRoutes = require('./routes/nutritionRoutes.js');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
-
-
-
 app.use(express.json());
 
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, 'Client', 'build')));
+
+// API endpoints
 app.use('/api/users', userRoutes);
-app.use('/api/protected', protectedRoutes); // Use the protected routes
+app.use('/api/protected', protectedRoutes);
 app.use('/api/nutrition', nutritionRoutes);
-app.use('/api/progress',progressRoutes);
-app.use('/api/workouts',workoutRoutes);
+app.use('/api/progress', progressRoutes);
+app.use('/api/workouts', workoutRoutes);
 
-
-
-
-
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
